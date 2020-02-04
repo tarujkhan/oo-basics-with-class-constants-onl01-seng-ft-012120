8aa7b3e524e7c7ded10b5b4d48fd113c988dd192
taroj khan <tarujkhan@yahoo.com>
2020-02-04 00:42:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,32 @@
+#class Shoe
+ # attr_accessor :color, :size, :material, :condition
+  #attr_reader :brand
+
+  #def initialize(brand)
+  #  @brand = brand
+  #end
+
+  #ef cobble
+   # self.condition = "new"
+    #puts "Your shoe is as good as new!"
+  #end
+
+#end
+
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
   end
 
   def cobble
